9f64128dc2cc9305ae7c0abb53c3163017872990
Victor Watanabe <victorhwmn@gmail.com>
2020-05-12 20:43:21 -0300

    Test

---
+++ build/precache-manifest.6981106db3dec9b4e2f73e3cebf6621f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c47df3590651dc93d5740c5c68b595",
+    "url": "./index.html"
+  },
+  {
+    "revision": "36316aa7bdba5d789229",
+    "url": "./static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "db88fedb9927a7145fbf",
+    "url": "./static/css/main.5ef99b63.chunk.css"
+  },
+  {
+    "revision": "36316aa7bdba5d789229",
+    "url": "./static/js/2.b745ba9e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.b745ba9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db88fedb9927a7145fbf",
+    "url": "./static/js/main.75a05f23.chunk.js"
+  },
+  {
+    "revision": "f585165c7f24d23ffb81",
+    "url": "./static/js/runtime-main.f886bc87.js"
+  },
+  {
+    "revision": "d336f5cbf0f69522b794306e0c4b0a5f",
+    "url": "./static/media/and.d336f5cb.jpg"
+  }
+]);
